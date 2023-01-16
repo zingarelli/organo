fd85def2ebc5a02e7fca80dc727ea3f14599f9ad
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-01-16 09:22:55 -0300

    added text and onChange as props

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,12 +1,17 @@
-import './TextInput.css';
+    import './TextInput.css';
 
 // a different coding style to create a function component
 // create a constant, which receives an arrow function, and export this constant
-const TextInput = ({required, label, placeholder}) => {
+const TextInput = ({required, label, placeholder, text, onChange}) => {
+
+    const onTyped = (e) => {
+        onChange(e.target.value);
+    }
+    
     return (
         <div className="text-field">
             <label htmlFor="">{label}</label>
-            <input type="text" placeholder={placeholder} required={required} />
+            <input value={text} type="text" placeholder={placeholder} required={required} onChange={onTyped}/>
         </div>
     );
 }
