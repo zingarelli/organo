39064024d39b96b707011b30ccccc59ef352ea12
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-01-16 12:43:45 -0300

    passing member data as prop

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,13 @@
+import Member from '../Member';
 import './Team.css';
 
-const Team = ({ name, primaryColor, secondaryColor }) => {
+const Team = ({ name, primaryColor, secondaryColor, members }) => {
     return (
-        <section className='teamContainer' style={{ backgroundColor: secondaryColor }} >
+        members.length > 0 && <section className='teamContainer' style={{ backgroundColor: secondaryColor }} >
             <h3 style={{ borderColor: primaryColor }}>{name}</h3>
+            <div className="memberList">
+                {members.map(member => <Member key={member.name} name={member.name} role={member.role} image={member.image} />)}
+            </div>
         </section>
     );
 }
