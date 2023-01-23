e473e77bfff806c73fa5368aa530b4dd23cf1f35
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-01-23 15:42:10 -0300

    generate id with uuid

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import Button from '../Button';
 import Dropdown from '../Dropdown';
@@ -13,7 +14,7 @@ const Form = ({ teams, onRegisteredMember }) => {
 
     const onSave = (e) => {
         e.preventDefault();
-        onRegisteredMember({name, role, image, team});
+        onRegisteredMember({id: uuidv4(), name, role, image, team});
         setName('');
         setRole('');
         setImage('');
